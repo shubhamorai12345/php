c490cad36f9a8d1a5ff1351e2e315ee547c806e6
shubham gupta <36784415+shubhamorai12345@users.noreply.github.com>
2018-11-15 22:07:08 +0530

    value pass 1 page to 2 page

---
+++ phpform.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>PHP FORM</title>
+</head>
+<body>
+<center>
+<form action="phpform1.php" method="get">
+<h3>FORM</h3>
+<input type="text" name="name" placeholder=" Full Name" required></br></br>
+<input type="num" name="dob" placeholder=" Date of birth" required></br></br>
+<input type="num" name="roll" placeholder=" Roll number" required></br></br>
+<input type="password" name="password" placeholder=" password" required></br></br>
+<input type="submit" name="submit">
+</form>
+</center>
+</body>
+</html>
---
+++ phpform1.php
@@ -0,0 +1,11 @@
+<?php
+
+$name=$_GET["name"];
+$dob=$_GET["dob"];
+$roll=$_GET["roll"];
+$password=$_GET["password"];
+
+
+echo ' Hello! your name '.$name.'  ,  your Data of Birth  '.$dob.'  , your Roll number '.$roll.'   and your password is    '.$password.'.';
+
+?>
